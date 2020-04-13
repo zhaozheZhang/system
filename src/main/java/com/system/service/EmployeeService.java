784c66f11affd46e52e49cc9dac63e21c508d2d8
zhaozheZhang <506549503@qq.com>
2020-04-13 18:06:42 +0800

    查询用户使用redis缓存

--- src/main/java/com/system/service/EmployeeService.java
+++ src/main/java/com/system/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.system.service;
 
 import com.system.mapper.EmployeeMapper;
 import com.system.pojo.Employee;
+import com.system.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +17,21 @@ public class EmployeeService {
 
     @Autowired
     EmployeeMapper employeeMapper;
+    @Autowired
+    RedisUtil redisUtil;
 
     public List<Employee> getEmployeeList(){
         return employeeMapper.queryEmployeeList();
     }
 
     public Employee getEmployee(Integer id){
-        return employeeMapper.queryById(id);
+        Employee employee = (Employee)redisUtil.get(id.toString());
+        if (employee != null){
+            return employee;
+        }
+        Employee employee1 = employeeMapper.queryById(id);
+        redisUtil.set(id.toString(), employee1);
+        return employee1;
     }
     public int addEmployee(Employee employee){
         return employeeMapper.addUEmployee(employee);
